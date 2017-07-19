b9dbf800ba55747ce7087755ef6e1b6181ee1007
Kirill Volovitskiy <kiv@sitecore.net>
2017-07-19 12:09:54 +0300

    Initial commit

---
+++ src/Sitecore.Support.96838/Speak/Applications/SelectMediaDialog.cs
@@ -0,0 +1,33 @@
+﻿using Sitecore.Diagnostics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sitecore.Support.Speak.Applications
+{
+    public class SelectMediaDialog : Sitecore.Speak.Applications.SelectMediaDialog
+    {
+        public override void Initialize()
+        {
+            base.Initialize();
+
+            Uri tempUri = null;
+            string lang = null;
+            try
+            {
+                tempUri = new Uri(HttpUtility.ParseQueryString(HttpContext.Current.Request.Url.Query)["ro"]);
+                lang = HttpUtility.ParseQueryString(tempUri.Query)["lang"];
+            }
+            catch (Exception e)
+            {
+                Log.Warn("Sitecore.Support.96838: " + e.Message, this);
+            }
+
+            if (!string.IsNullOrEmpty(lang))
+            {
+                base.DataSource.Parameters["Language"] = lang;
+            }
+        }
+    }
+}
